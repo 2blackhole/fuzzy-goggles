24575b389700760c7205a42013852590a0734a9e
2blackhole <barabanegor2000@gmail.com>
2025-04-20 11:18:27 +0300

    Test class summator. All set now

--- camera/src/register_types.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Lenovo on 2025-04-16.
-//
-
-#include "register_types.h"
---
+++ normik/camera_3d_2.gd
@@ -0,0 +1,7 @@
+extends Camera3D
+
+func _ready() -> void:
+	var summator = Summator.new();
+	summator.add(3);
+	summator.add(4);
+	print(summator.get_total());
