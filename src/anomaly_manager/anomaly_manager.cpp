16d0f923571f1372513cd7a0c5f7a0fba5f01586
2blackhole <barabanegor2000@gmail.com>
2025-05-13 00:15:13 +0300

    OK???

--- src/anomaly_manager/anomaly_manager.cpp
+++ src/anomaly_manager/anomaly_manager.cpp
@@ -5,8 +5,6 @@
 void AnomalyManager::_ready() {
     TypedArray vec = get_children();
 
-    print_line(vec.size());
-
     for (int i = 0; i < vec.size(); ++i) {
         Anomaly* el = cast_to<Anomaly>(vec[i]);
         if (el) {
@@ -15,8 +13,9 @@ void AnomalyManager::_ready() {
         }
     }
     anomalies_count = anomalies.size();
-    print_line(active_anomalies_count);
-    print_line(max_active_anomalies);
+    print_line("anomalies_count: ", anomalies_count);
+    print_line("active_anomalies_count: ", active_anomalies_count);
+    print_line("max_active_anomalies: ", max_active_anomalies);
 }
 
 void AnomalyManager::on_anomaly_deactivated() {
