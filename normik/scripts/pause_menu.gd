421ab8ed839347b071d8f31e6598ce9c7ea825d0
2blackhole <barabanegor2000@gmail.com>
2025-05-12 20:57:18 +0300

    ne lomaysia

---
+++ normik/scripts/pause_menu.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+func _ready() -> void:
+	hide();
+	get_tree().paused = false;
+	
+	
+
+
+func _on_continue_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_to_menu_pressed() -> void:
+	pass # Replace with function body.
