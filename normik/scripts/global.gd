bd0514f5589ce0104ba0eb1150ea37ceffdb95ba
2blackhole <barabanegor2000@gmail.com>
2025-05-14 20:30:19 +0300

    privet

---
+++ normik/scripts/global.gd
@@ -0,0 +1,4 @@
+# global.gd
+extends Node
+
+var return_scene_path: String = "res://main_menu.tscn"  # сцена по умолчанию
--- normik/scripts/pause_menu.gd
+++ normik/scripts/pause_menu.gd
@@ -1,34 +1,37 @@
+# pause_menu.gd
 extends CanvasLayer
-var time = 0;
+var time = 0
+
 func _ready() -> void:
-	hide();
-	get_tree().paused = false;
-	process_mode = Node.PROCESS_MODE_ALWAYS  # Это позволит ноду обрабатывать ввод даже в паузе
+	hide()
+	get_tree().paused = false
+	process_mode = Node.PROCESS_MODE_ALWAYS
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
-		print('jopa')
-		if (visible):
+		if visible:
 			hide()
 			get_tree().paused = false
 		else:
 			show()
 			get_tree().paused = true
+			# Устанавливаем сцену возврата как текущую сцену
+			Global.return_scene_path = get_tree().current_scene.scene_file_path
 
 func _process(delta: float) -> void:
-	var cont = $ColorRect/MarginContainer;
-	time += delta;
-	cont.rotation_degrees += abs(sin(time)) / 2;
+	var cont = $ColorRect/MarginContainer
+	time += delta
+	cont.rotation_degrees += abs(sin(time)) / 2
 	cont.scale = Vector2(abs(sin(time)) * 2.4, abs(cos(time)) * 3.4)
 
 func _on_continue_pressed() -> void:
 	get_tree().paused = false
 	hide()
 
-
 func _on_settings_pressed() -> void:
-	pass # Replace with function body.
-
+	# Сохраняем текущую сцену перед переходом в настройки
+	Global.return_scene_path = get_tree().current_scene.scene_file_path
+	get_tree().change_scene_to_file("res://settings_menu.tscn")
 
 func _on_exit_to_menu_pressed() -> void:
 	get_tree().paused = false
