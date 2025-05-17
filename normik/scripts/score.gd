eb5b8ad87878c463fd28a260a5d430e9c2518362
2blackhole <barabanegor2000@gmail.com>
2025-05-17 14:37:24 +0300

    pusto

--- normik/scripts/score.gd
+++ normik/scripts/score.gd
@@ -15,7 +15,10 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	score.text = "Score: " + str($"..".get_score())
 	var minut = str(int(govno / 60))
-	var sekund = str(int(govno) % 60);
+	var sekund = int(govno) % 60;
+	if sekund <= 9:
+		sekund = str("0" + str(sekund))
+	sekund = str(sekund)
 	time_left.text = "Time left - " + minut + ":" + sekund;
 	govno -= delta
 	if govno <= 0:
